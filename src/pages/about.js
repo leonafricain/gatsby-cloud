4fad3f9febe1825f0e4e1aa45c924bfa0af31c54
leonafricain <kimleonafricain@gmail.com>
2021-01-16 22:14:10 +0100

    second

--- src/pages/about.js
+++ src/pages/about.js
@@ -4,8 +4,8 @@ import Header from "../components/header"
 export default function About() {
   return (
     <div style={{ color: `teal` }}>
-      <Header title="About Gatsby!!!" />
-      <Header title="another About Gatsby!!!" />
+      <Header title="About Gatsby!!! header 1" />
+      <Header title="another About Gatsby!!! header 2" />
 
       <p>Such wow. Very React.</p>
     </div>
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,11 +5,12 @@ import Header from "../components/header"
 export default function Home() {
   return (
     <div style={{ color: "purple" }}>
-      <Link to="/contact/">Contact</Link>
+      <Link style={{marginRight: '15px'}} to="/contact/">Contact</Link>
+      <Link to="/about/">About</Link>
       <h1>Hello Gatsby!</h1>
       <p>What a world</p>
-      <img src="https://source.unsplash.com/random/" alt="" />
-      <Header />
+      <img src="https://source.unsplash.com/random/450x200" alt="" />
+      
     </div>
   )
 }
